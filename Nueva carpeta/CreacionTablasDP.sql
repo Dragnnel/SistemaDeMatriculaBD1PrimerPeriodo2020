e02ed82436007d2d988a1e0ca6c81c7049d325e9
Palacios <37627873+davidpalacios18@users.noreply.github.com>
2020-03-26 22:28:41 -0600

    Datos Personales

--- Nueva carpeta/CreacionTablasDP.sql
+++ Nueva carpeta/CreacionTablasDP.sql
@@ -67,3 +67,77 @@ CREATE TABLE Facultad(idFacultad INT PRIMARY KEY NOT NULL,
 					  FOREIGN KEY(idEdificio) REFERENCES Edificio(idEdificio),
 					  FOREIGN KEY(centroUniversitario) REFERENCES CentroUniversitario(idCentro)
 					  )
+
+/*
+	DATOS PERSONALES
+*/
+CREATE TABLE GruposEtnico(idGrupoEtnico INT PRIMARY KEY NOT NULL,
+						  descripcion VARCHAR(15)
+						  )
+
+CREATE TABLE TipoDiscapacidad(idDiscapacidad INT PRIMARY KEY NOT NULL,
+							  descripcion VARCHAR(15)
+							  )
+
+CREATE TABLE Nacionalidad(idNacionalidad INT PRIMARY KEY NOT NULL,
+						  descripcion VARCHAR(45)
+						  )
+
+CREATE TABLE EstadoCivil(idEstado INT PRIMARY KEY NOT NULL,
+						 descripcion VARCHAR(15)
+						 )
+
+CREATE TABLE Sexo(idSexo INT PRIMARY KEY NOT NULL,
+				  descripcion VARCHAR(15)
+				  )
+
+CREATE TABLE Parentesco(idParentesco INT PRIMARY KEY NOT NULL,
+						descripcion VARCHAR(45)
+						)
+
+CREATE TABLE Referencia(idReferencia INT PRIMARY KEY NOT NULL,
+						nombreCompleto VARCHAR(100),
+						idParentesco INT,
+						FOREIGN KEY(idParentesco) REFERENCES Parentesco(idParentesco)
+						)
+
+CREATE TABLE Correo(idCorreo INT PRIMARY KEY NOT NULL,
+					correoPersonal VARCHAR(45),
+					correoInstitucional VARCHAR(45)
+					)
+
+CREATE TABLE Persona(idPersona VARCHAR(15) PRIMARY KEY NOT NULL, --Teniendo esto por numero de identidad
+					 primerNombre VARCHAR(35),
+					 segundoNombre VARCHAR(35),
+					 primerApellido VARCHAR(35),
+					 segundoApellido VARCHAR(35),
+					 fechaNacimiento DATE,
+					 idDireccion INT,
+					 idNacionalidad INT,
+					 idGupoEtnico INT,
+					 idSexo INT,
+					 idDiscapacidad INT,
+					 idEstadoCivil INT,
+					 idCorreo INT,
+					 idReferencia INT,
+					 FOREIGN KEY(idDireccion) REFERENCES Direccion(idDireccion),
+					 FOREIGN KEY(idNacionalidad) REFERENCES Nacionalidad(idNacionalidad),
+					 FOREIGN KEY(idGupoEtnico) REFERENCES GruposEtnico(idGrupoEtnico),
+					 FOREIGN KEY(idSexo) REFERENCES Sexo(idSexo),
+					 FOREIGN KEY(idDiscapacidad) REFERENCES TipoDiscapacidad(idDiscapacidad),
+					 FOREIGN KEY(idEstadoCivil) REFERENCES EstadoCivil(idEstado),
+					 FOREIGN KEY(idCorreo) REFERENCES Correo(idCorreo),
+					 FOREIGN KEY(idReferencia) REFERENCES Referencia(idReferencia)
+					 )
+
+CREATE TABLE Telefono(idTelefono INT PRIMARY KEY NOT NULL,
+					  telefono VARCHAR(15),
+					  idPersona VARCHAR(15),
+					  idReferencia INT,
+					  FOREIGN KEY(idPersona) REFERENCES Persona(idPersona),
+					  FOREIGN KEY(idReferencia) REFERENCES Referencia(idReferencia)
+					  )
+
+/*
+	
+*/
