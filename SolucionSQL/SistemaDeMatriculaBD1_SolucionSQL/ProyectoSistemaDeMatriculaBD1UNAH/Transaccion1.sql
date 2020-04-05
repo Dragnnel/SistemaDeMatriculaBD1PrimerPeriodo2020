d9fbed9d45771ea1f039790cee81cf0686ea551e
Palacios <37627873+davidpalacios18@users.noreply.github.com>
2020-04-05 09:17:35 -0600

    Transacciones

--- SolucionSQL/SistemaDeMatriculaBD1_SolucionSQL/ProyectoSistemaDeMatriculaBD1UNAH/Transaccion1.sql
+++ SolucionSQL/SistemaDeMatriculaBD1_SolucionSQL/ProyectoSistemaDeMatriculaBD1UNAH/Transaccion1.sql
@@ -78,7 +78,8 @@ END CATCH
 --------------------------------------------------------------------------------------------------------------------
 /*
 	TRANSACCION #2 : Transaccion para reducir el numero de cupos en una seccion.
-					 *Aqui se tendra que registrar una nueva matricula con una seccion existente
+					 *Aqui se inserta un registro en la tabla "SeccionMatricula", y al realizarce correctamente
+					  reduce en uno los cupos disponibles en la seccion de la asignatura correspondiente
 */
 --------------------------------------------------------------------------------------------------------------------
 /*
@@ -98,9 +99,134 @@ SELECT *
 	FROM ProyectoSistemaMatricula.unah.Matricula
 */
 
-CREATE PROCEDURE SP_InsertSeccionMatricula(
+ALTER PROCEDURE SP_InsertSeccionMatricula(
 			@idMatricula INT,
 			@idSeccion INT,
 			@idAsignatura INT,
-			@notaFinal
+			@notaFinal DECIMAL(2, 2),
+			@idObservacion INT
 		)
+AS
+SET NOCOUNT ON
+
+IF NOT EXISTS(SELECT *
+				FROM ProyectoSistemaMatricula.unah.SeccionMatricula SM
+			   WHERE SM.idMatricula = @idMatricula
+				 AND SM.idSeccion = @idSeccion
+				 AND SM.idAsignatura = @idAsignatura
+			  )
+	BEGIN
+		BEGIN TRY
+			BEGIN TRANSACTION
+				INSERT INTO ProyectoSistemaMatricula.unah.SeccionMatricula(idMatricula,
+																   idSeccion,
+																   idAsignatura,
+																   notaFinal,
+																   idObservacionNota
+																   )
+															VALUES(@idMatricula,
+																   @idSeccion,
+																   @idAsignatura,
+																   @notaFinal,
+																   @idObservacion
+																   )	
+
+			IF @@ROWCOUNT = 1
+				BEGIN
+					IF (SELECT T1.cuposDisponibles 
+							FROM ProyectoSistemaMatricula.unah.Seccion T1
+						   WHERE idSeccion = @idSeccion
+						     AND idAsignatura = @idAsignatura) > 0
+						BEGIN
+							UPDATE ProyectoSistemaMatricula.unah.Seccion
+								SET cuposDisponibles = cuposDisponibles - 1
+							  WHERE idSeccion = @idSeccion
+								AND idAsignatura = @idAsignatura
+						
+							COMMIT TRANSACTION
+
+							SELECT 1 AS RESULTADO
+							PRINT 'La matricula se registro CORRECTAMENTE'
+						END
+				END
+
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRANSACTION
+
+			SELECT 0 AS RESULTADO
+			PRINT 'No se pudo registrar la matricula'
+		END CATCH
+	END
+ELSE
+	BEGIN
+		PRINT 'La Asignatura(Sección) ya está Matriculada'
+	END
+
+
+
+--------------------------------------------------------------------------------------------------------
+/*
+	TRANSACCION #3: Transaccion con la que se actualizaran los datos de un determinado Estudiante
+*/
+--------------------------------------------------------------------------------------------------------
+/*
+SELECT *
+	FROM ProyectoSistemaMatricula.unah.Correo
+
+SELECT * 
+	FROM ProyectoSistemaMatricula.unah.Persona
+*/
+
+ALTER PROCEDURE SP_UpdateCorreo(
+		@idPersona VARCHAR(15),
+		@idCorreo INT,
+		@TipoCorreo INT, -- 0, 1; 0 = personal, 1 = Institucional
+		@correo VARCHAR(45)
+	)
+AS
+SET NOCOUNT ON
+
+IF EXISTS(
+			Select *
+				      FROM ProyectoSistemaMatricula.unah.Persona T1
+				INNER JOIN ProyectoSistemaMatricula.unah.Correo T2
+						ON T1.idCorreo = T2.idCorreo
+					 WHERE T1.idPersona = @idPersona
+					   AND T1.idCorreo = @idCorreo
+		  )
+	BEGIN
+		BEGIN TRY
+			BEGIN TRANSACTION
+				IF @TipoCorreo = 0
+					BEGIN
+						UPDATE ProyectoSistemaMatricula.unah.Correo
+							SET correoPersonal = @correo
+						  WHERE idCorreo = @idCorreo
+
+						 COMMIT TRANSACTION
+						 SELECT 1 AS RESULTADO
+					END
+				ELSE
+					BEGIN
+						IF @TipoCorreo = 1
+						BEGIN
+							UPDATE ProyectoSistemaMatricula.unah.Correo
+								SET correoInstitucional = @correo
+							  WHERE idCorreo = @idCorreo
+
+							COMMIT TRANSACTION
+							SELECT 1 AS RESULTADO
+						END
+					END
+		END TRY
+		BEGIN CATCH
+			ROLLBACK TRANSACTION
+			
+			SELECT 0 AS RESULTADO
+		END CATCH
+	END
+ELSE
+	BEGIN
+		PRINT 'La persona o correo no estan registrados, Verifique los datos ingresados'
+	END
