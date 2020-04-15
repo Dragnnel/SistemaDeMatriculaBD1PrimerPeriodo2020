0cdb39fce574e340ec02e1f46fd7a17276fe057f
Luis Estrada <lfe9621@gmail.com>
2020-04-14 23:01:24 -0600

    Actualizacioni de propuestas para procedimiento

---
+++ Ejemplo de insertar matricula/propuestas.sql
@@ -0,0 +1,48 @@
+/*
+
+Que procedimientos o funcionres realizar 
+ 
+ 1- Crear una seccion 
+
+ 2- Modificar una seccion 
+
+ 3- Cancelar una seccion 
+
+ 4- Matricula de laboratrio
+
+ 5- Modificar laboratrio
+
+ 6- Cancelar laboratrio
+
+ 7- ingreso de estudiantes 
+
+ 8- modificar 
+
+ 9- ingreso de empleado
+
+ 10-verificar requisitos de si pago la matricula
+
+ 11-verificar si cumple , los requisitos para la asignatura
+
+ 12-
+
+
+
+*/
+
+
+/*
+
+Vistas a realizar 
+
+
+1- Imprimir historial academico (ver si por procedimientos o por vistas )
+
+2- Ver forma 003
+
+3- Ver las clases que cancelo 
+
+4- ver las clases que tiene en espera
+
+5-
+*/
